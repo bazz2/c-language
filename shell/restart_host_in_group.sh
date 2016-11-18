92214dcc6b2b156fee976d792b9a6b5eecf32e9c
bazz2 <chenjt1989@sina.com>
2016-11-18 13:28:58 +0800

    add -f

--- shell/restart_host_in_group.sh
+++ shell/restart_host_in_group.sh
@@ -23,7 +23,7 @@ group="failover-APP"
 hosts=("APP01" "APP02")
 dst_host=`echo "$all_group" |grep $group -A1 |grep current |awk '{print $3}'`
 if [[ "${hosts[@]}" =~ "$dst_host" ]]; then
-    clpgrp -t $group
+    clpgrp -t $group -f
     clpgrp -s $group -h $dst_host
 fi
 
@@ -31,7 +31,7 @@ group="failover-WEB"
 hosts=("WEB01" "WEB02")
 dst_host=`echo "$all_group" |grep $group -A1 |grep current |awk '{print $3}'`
 if [[ "${hosts[@]}" =~ "$dst_host" ]]; then
-    clpgrp -t $group
+    clpgrp -t $group -f
     clpgrp -s $group -h $dst_host
 fi
 
